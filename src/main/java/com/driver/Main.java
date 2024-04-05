c061581b9f9ad87615a4837b18bd3235506002c0
Arvapalli-Ramprasad <155809700+Arvapalli-Ramprasad@users.noreply.github.com>
2024-04-05 13:22:26 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,9 +1,6 @@
 package com.driver;
 // Task 1: Create a class A with a method named meth returning a string: "Invoking method from class A"
 
-import com.driver.A; // Import class A
-import com.driver.B; // Import class B
-
 public class Main {
   public static void main(String[] args) {
         // Task 3: Create an object of class B and call method meth of class A with it
