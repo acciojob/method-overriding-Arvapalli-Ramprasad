e9fcfbbd98460d53829c793f336ac94db59eabed
Arvapalli-Ramprasad <155809700+Arvapalli-Ramprasad@users.noreply.github.com>
2024-04-05 14:03:07 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -4,5 +4,10 @@ import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-    
+    A objA = new A();
+        B objB = new B();
+        
+        assertEquals("Invoking method from class A", objA.meth());
+        assertEquals("Method is overridden in Extended class B", objB.meth());
+    }
 }
