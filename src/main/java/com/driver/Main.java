af46fd476a937ae793375fe4aba25ce8ae455aef
Arvapalli-Ramprasad <155809700+Arvapalli-Ramprasad@users.noreply.github.com>
2024-04-06 21:02:44 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -21,7 +21,6 @@ public class Main {
     // Task 2: Create a static nested class B which extends class A
     public static class B extends A {
         // Task 4: Override method meth of class A in class B
-        @Override
         public String meth() {
             return "Method is overridden in Extended class B";
         }
