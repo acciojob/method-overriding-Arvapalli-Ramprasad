c59990ed80163ae1ffae62ad7e14ff3ce7134476
Arvapalli-Ramprasad <155809700+Arvapalli-Ramprasad@users.noreply.github.com>
2024-04-06 21:02:11 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,14 +1,10 @@
 package com.driver.test;
-
-// Import necessary classes
 import com.driver.A;
 import com.driver.B;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestCases {
-
-    @Test
     public void testMethodOverride() {
         // Create an object of class B
         B objB = new B();
