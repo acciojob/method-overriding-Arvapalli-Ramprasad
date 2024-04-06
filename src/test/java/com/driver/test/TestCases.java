64a57dd138f267c51b9427786d5708a171fe5fc6
Arvapalli-Ramprasad <155809700+Arvapalli-Ramprasad@users.noreply.github.com>
2024-04-06 20:02:58 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -3,24 +3,6 @@ package com.driver.test;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-// Import classes A and B
-import com.driver.A;
-import com.driver.B;
-import com.driver.Main;
-
-
 public class TestCases {
-
-    @Test
-    public void testMethodOverride() {
-        // Create objects of classes A and B
-        A objA = new A();
-        B objB = new B();
-        
-        // Test method meth() from class A
-        assertEquals("Invoking method from class A", objA.meth());
-        
-        // Test method meth() from class B
-        assertEquals("Method is overridden in Extended class B", objB.meth());
     }
 }
