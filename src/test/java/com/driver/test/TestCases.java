023af00406e9a7ffdfe1ec754cd08668ba9af82d
Arvapalli-Ramprasad <155809700+Arvapalli-Ramprasad@users.noreply.github.com>
2024-04-10 10:07:42 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,7 +1,8 @@
 package com.driver.test;
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
 
 public class TestCases {
-    
+     
 }
