2d9bf997f7f4bfe544a0fb0a7e5998705d0ea5c1
Arvapalli-Ramprasad <155809700+Arvapalli-Ramprasad@users.noreply.github.com>
2024-04-10 11:13:59 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -4,10 +4,4 @@ import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-    // Task 5: Add a test case to test the overridden method from object of class B
-    @Test
-    public void testprintHelloWorld2() {
-        Main.B objB = new Main.B();
-        assertEquals(objB.meth());
-    }
 }
