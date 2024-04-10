0097b281ea49253ab3d40ed7a89fc9c97ea6481b
Arvapalli-Ramprasad <155809700+Arvapalli-Ramprasad@users.noreply.github.com>
2024-04-10 11:28:36 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,9 +1,7 @@
 package com.driver;
 public class Main {
     public static void main(String[] args) {
-     
         B objB = new B();
-        
         objB.meth();
     }
     public static class A {
