9d72ac3c0c1f5fbf9186c6eb49fd4dc4fd24ae9f
Arvapalli-Ramprasad <155809700+Arvapalli-Ramprasad@users.noreply.github.com>
2024-04-10 09:57:27 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,28 +1,28 @@
-//package com.driver;
+package com.driver;
+
+// Task 1: Create a class A with a method named meth returning a string: "Invoking method from class A"
+class A {
+    public String meth() {
+        return "Invoking method from class A";
+    }
+}
+
+// Task 2: Create a class B which extends class A
+class B extends A {
+    // Task 4: Override method meth of class A in class B
+    @Override
+    public String meth() {
+        return "Method is overridden in Extended class B";
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        // Creating an instance of Main
-        Main mainInstance = new Main();
-        
         // Task 3: Create an object of class B and call method meth of class A with it
         B objB = new B();
+        System.out.println(objB.meth()); // Output will be "Invoking method from class A"
+        
         // Task 5: Call this overridden method from object of class B
         System.out.println(objB.meth()); // Output will be "Method is overridden in Extended class B"
-    }
-
-    // Task 1: Create a static nested class A with a method named meth returning a string: "Invoking method from class A"
-    public static class A {
-        public String meth() {
-            return "Invoking method from class A";
-        }
-    }
-
-    // Task 2: Create a static nested class B which extends class A
-    public static class B extends A {
-        // Task 4: Override method meth of class A in class B
-        public String meth() {
-            return "Method is overridden in Extended class B";
-        }
     }
 }
